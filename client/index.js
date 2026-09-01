5ac483b8bec3db665f6717db10fbf71410717ea9
coderaiser <coderaiser@cloudcmd.io>
2026-09-01 14:36:00 +0000

    feat: index: add barrel export

---
+++ client/index.js
@@ -0,0 +1,32 @@
+export {createEditor} from './create.js';
+export {
+    getValue,
+    setValue,
+    getDocValue,
+    setDocValue,
+} from './content.js';
+export {
+    offsetToPosition,
+    positionToOffset,
+    posFromIndex,
+    indexFromPos,
+    getCursorIndex,
+} from './position.js';
+export {
+    markField,
+    lineField,
+    markText,
+    addLineClass,
+    removeLineClass,
+} from './decorations.js';
+export {on, off} from './events.js';
+export {getScrollInfo, scrollTo} from './scroll.js';
+export {getView, refresh, observeResize} from './dom.js';
+export {
+    keymapExtension,
+    themeExtension,
+    languageExtension,
+    setOption,
+} from './options.js';
+export {clearHistory} from './history.js';
+export {highlightStyle} from './highlight.js';
