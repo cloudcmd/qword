d4896d3d2f2907a9f25f2c9791403010781f2a07
coderaiser <coderaiser@cloudcmd.io>
2026-09-01 14:34:09 +0000

    feat: create: extract EditorView factory with highlight + history

---
+++ client/create.js
@@ -0,0 +1,78 @@
+import {EditorState, Compartment} from '@codemirror/state';
+import {
+    EditorView,
+    lineNumbers as lineNumbersExtension,
+    drawSelection,
+    keymap,
+} from '@codemirror/view';
+import {history, historyKeymap} from '@codemirror/commands';
+import {
+    foldGutter,
+    codeFolding,
+    syntaxHighlighting,
+} from '@codemirror/language';
+import {highlightStyle} from './highlight.js';
+import {
+    keymapExtension,
+    themeExtension,
+    languageExtension,
+} from './options.js';
+import {markField, lineField} from './decorations.js';
+
+export function createEditor(element, options = {}) {
+    const {
+        value = '',
+        mode = 'javascript',
+        keyMap = 'default',
+        theme = 'default',
+        lineNumbers = true,
+        readOnly = false,
+        foldGutter: fold = false,
+        updateListener,
+    } = options;
+
+    const themeCompartment = new Compartment();
+    const keymapCompartment = new Compartment();
+    const langCompartment = new Compartment();
+    const historyCompartment = new Compartment();
+
+    const hideCursorOnBlur = EditorView.theme({
+        '&:not(.cm-focused) .cm-fat-cursor': {
+            display: 'none',
+        },
+    });
+
+    const extensions = [
+        historyCompartment.of([
+            history(),
+            keymap.of(historyKeymap),
+        ]),
+        markField,
+        lineField,
+        themeCompartment.of(themeExtension(theme)),
+        keymapCompartment.of(keymapExtension(keyMap)),
+        langCompartment.of(languageExtension(mode)),
+        syntaxHighlighting(highlightStyle),
+        ...lineNumbers ? [lineNumbersExtension()] : [],
+        ...fold ? [foldGutter(), codeFolding()] : [],
+        ...readOnly ? [EditorState.readOnly.of(true)] : [],
+        ...updateListener ? [EditorView.updateListener.of(updateListener)] : [],
+        drawSelection(),
+        hideCursorOnBlur,
+    ];
+
+    const view = new EditorView({
+        state: EditorState.create({
+            doc: value,
+            extensions,
+        }),
+        parent: element,
+    });
+
+    view._themeCompartment = themeCompartment;
+    view._keymapCompartment = keymapCompartment;
+    view._langCompartment = langCompartment;
+    view._historyCompartment = historyCompartment;
+
+    return view;
+}
